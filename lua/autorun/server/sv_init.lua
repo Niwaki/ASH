26f3fbe5826f7fc0de6c82014858eab2db7eac28
How is this not taken <bamgaming06@gmail.com>
2017-12-11 22:12:19 -0800

    Update sv_init.lua

--- lua/autorun/server/sv_init.lua
+++ lua/autorun/server/sv_init.lua
@@ -3,8 +3,8 @@
 	Last Modified by: jackool.
 --]]
 
-util.AddNetworkString('ban')
+util.AddNetworkString('ash_ban')
 
-net.Receive("ban", function(len, ply)
+net.Receive("ash_ban", function(len, ply)
     ply:Ban(0, "Goodbye!") // You can change the ban time, the value "0" = forever, and you can change the reason ban.
 end)
