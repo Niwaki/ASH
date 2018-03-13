29033c840f1c73f475ab58b69b32c65ac98f81b0
Niwaki <daniel.gumarov@bk.ru>
2018-03-13 11:39:06 +0300

    Update sv_init.lua

--- lua/autorun/server/sv_init.lua
+++ lua/autorun/server/sv_init.lua
@@ -1,10 +1,18 @@
---[[
+--[[ 
 	© 2017 Niwaka
 	Last Modified by: Niwaka.
 --]]
 
-util.AddNetworkString('ash_ban')
+util.AddNetworkString( 'ash_ban' );
 
-net.Receive("ash_ban", function(len, ply)
-    RunConsoleCommand("ulx", "banid", ply:SteamID(), 0, "Reason") // You can change the ban time in, the value "0" = forever, and you can change the reason ban.
-end) 
+--[[ ULX BAN ]]--
+
+net.Receive( 'ash_ban', function( len, ply )
+    RunConsoleCommand( 'ulx', 'banid', ply:SteamID(), 0, 'Reason' ); // You can change the ban time in, the value "0" = forever, and you can change the reason ban.
+end);
+
+--[[ GMOD BAN 
+net.Receive( 'ash_ban', function( len, ply )
+    ply:Ban( 0, 'Reason' ); // You can change the ban time in, the value "0" = forever, and you can change the reason ban.
+end);
+--]]
